0d77c2998529c08e1f7f4d4eab691684562b6646
Laura Caroselli <lcaroselli@outlook.com>
2017-10-13 01:24:30 -0600

    Add test data

--- db/seeds/test/tests.js
+++ db/seeds/test/tests.js
@@ -1,13 +1,53 @@
+const types = require('../../../type-test-data.json');
+const pokemon = require('../../../pokemon-test-data.json');
+
+const createType = (knex, type) => {
+  return knex('types').insert({
+    type_label: type.type_label
+  }, 'id');
+}
+
+const createPokemon = (knex, pokemon) => {
+  return knex('pokemon').insert(pokemon);
+}
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('pokemon').del()
+  .then(() => knex('types').del())
+
+  .then(() => {
+    let typesPromises = [];
+
+    types.forEach(type =>
+      typesPromises.push(createType(knex, type))
+    );
+
+    return Promise.all(typesPromises);
+  })
+
+  .then(type_id => {
+    let pokemonPromises = [];
+
+    pokemon.forEach(pokemon => {
+      pokemonPromises.push(createPokemon(knex, {
+        name: pokemon['name'],
+        region_id: pokemon['id'],
+        attack_power: pokemon['pokemonAttackPower'],
+        defense_power: pokemon['pokemonDefensePower'],
+        hp: pokemon['pokemonHp'],
+        power_total: pokemon['pokemonTotalStat'],
+        primary_type: pokemon['primaryType'],
+        type_id: knex
+          .select('id')
+          .from('types')
+          .where('type_label', pokemon['primaryType'])
+      }));
+    })
+
+    return Promise.all(pokemonPromises);
+  })
+
+  .then(() => { console.log('Seeding complete.') } )
+
+  .catch(error => console.log(`Error seeding data: ${error}`))
 };
