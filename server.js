364f598b8323840879576b5282c60e65e7962af0
Laura Caroselli <lcaroselli@outlook.com>
2017-10-13 18:57:43 -0600

    Lint server file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-require('dotenv').config()
+require('dotenv').config();
 const secretKey = process.env.secretKey;
 
 
@@ -21,7 +21,7 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Poke150 API';
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
 });
 
 
@@ -35,13 +35,13 @@ const checkAuth = (request, response, next) => {
 
   jwt.verify(token, app.get('secretKey'), (error, decoded) => {
     if (error) {
-      response.status(403).json({ error: 'Invalid token' });
+      response.status(403).json({ error: 'Invalid token' })
     }
 
     if (!decoded.admin) {
       response.status(403).json({ error: 'You are not authorized as admin to this endpoint' })
     }
-      next();
+    next()
   })
 };
 
@@ -49,14 +49,14 @@ const checkToken = (request, response, next) => {
   const token = request.body.token;
 
   if (!token) {
-    response.status(403).json({ error: 'You must be authorized to hit this endpoint' });
+    response.status(403).json({ error: 'You must be authorized to hit this endpoint' })
   }
 
   jwt.verify(token, app.get('secretKey'), (error, decoded) => {
     if (error) {
-      return response.status(403).json({ error: 'Invalid token' });
+      return response.status(403).json({ error: 'Invalid token' })
     }
-      next();
+    next()
   })
 };
 
@@ -77,17 +77,17 @@ app.get('/api/v1/pokemon', (request, response) => {
 
   checkQuery()
 
-  .then(pokemon => {
-    if (!pokemon.length) {
-      return response.status(404).json({ error: 'No pokemon found' })
-    }
-    response.status(200).json(pokemon)
-  })
+    .then(pokemon => {
+      if (!pokemon.length) {
+        return response.status(404).json({ error: 'No pokemon found' })
+      }
+      response.status(200).json(pokemon)
+    })
 
-  .catch(error => { response.status(500).json({ error }) })
+    .catch(error => { response.status(500).json({ error }) })
 });
 
-  //GET all types
+//GET all types
 app.get('/api/v1/types', (request, response) => {
   let { type_label } = request.query;
 
@@ -101,56 +101,56 @@ app.get('/api/v1/types', (request, response) => {
 
   checkQuery()
 
-  .then(types => {
-    if (!types.length) {
-      return response.status(404).json({ error: 'No pokemon types found' })
-    }
-    response.status(200).json(types)
-  })
+    .then(types => {
+      if (!types.length) {
+        return response.status(404).json({ error: 'No pokemon types found' })
+      }
+      response.status(200).json(types)
+    })
 
-  .catch(error => { response.status(500).json({ error }) })
+    .catch(error => { response.status(500).json({ error }) })
 });
 
-  //GET Pokemon by region id
+//GET Pokemon by region id
 app.get('/api/v1/pokemon/:region_id', (request, response) => {
   const { region_id } = request.params;
 
   database('pokemon').where({ region_id }).select()
 
-  .then(region => {
-    if (!region.length) {
-      return response.status(404).json({ error: 'No pokemon with that region id found' })
-    }
-    response.status(200).json(region)
-  })
+    .then(region => {
+      if (!region.length) {
+        return response.status(404).json({ error: 'No pokemon with that region id found' })
+      }
+      response.status(200).json(region)
+    })
 
-  .catch(error => {
-    response.status(500).json({ error })
-  })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
 });
 
-  //GET type by id
+//GET type by id
 app.get('/api/v1/types/:id', (request, response) => {
   const { id } = request.params;
 
   database('types').where({ id }).select()
 
-  .then(id => {
-   if (!id.length) {
-    return response.status(404).json({ error: 'No type with that id found' })
-   }
-   response.status(200).json(id)
- })
+    .then(id => {
+      if (!id.length) {
+        return response.status(404).json({ error: 'No type with that id found' })
+      }
+      response.status(200).json(id)
+    })
 
-  .catch(error => {
-    response.status(500).json({ error })
-  })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
 });
 
 
 
 //POST Endpoints
-  //POST for authentication
+//POST for authentication
 app.post('/api/v1/authenticate', (request, response) => {
   const { email, app } = request.body;
 
@@ -164,16 +164,16 @@ app.post('/api/v1/authenticate', (request, response) => {
     Object.assign(request.body, { admin: false })
   }
 
-	jwt.sign(request.body, secretKey, { expiresIn: '48h' }, (error, token) => {
-		if(token) {
+  jwt.sign(request.body, secretKey, { expiresIn: '48h' }, (error, token) => {
+    if(token) {
       response.status(200).json({ token })
     } else {
       response.status(404).json({ error })
     }
-	})
+  });
 });
 
-  //POST new type
+//POST new type
 app.post('/api/v1/types', checkAuth, (request, response) => {
   const { type_label } = request.body;
 
@@ -183,28 +183,28 @@ app.post('/api/v1/types', checkAuth, (request, response) => {
 
   database('types').insert({ type_label }, '*')
 
-  .then(type => response.status(201).json(type))
+    .then(type => response.status(201).json(type))
 
-  .catch(error => response.status(500).json({ error }))
+    .catch(error => response.status(500).json({ error }))
 });
 
-  //POST new pokemon
+//POST new pokemon
 app.post('/api/v1/pokemon', checkAuth, (request, response) => {
   const newPokemon = request.body;
 
   for (let pokeParameters of
     [ 'region_id', 'name', 'attack_power', 'defense_power', 'hp', 'power_total', 'type_id', 'primary_type' ]) {
 
-      if (!newPokemon[pokeParameters]) {
-        return response.status(422).send({ error: `Expected parameters: { region_id: <String>, name: <String>, attack_power: <String>, defense_power: <String>, hp: <String>, power_total: <String>, type_id: <Integer>, primary_type: <String> }. You're missing a ${pokeParameters}.` });
-      }
+    if (!newPokemon[pokeParameters]) {
+      return response.status(422).send({ error: `Expected parameters: { region_id: <String>, name: <String>, attack_power: <String>, defense_power: <String>, hp: <String>, power_total: <String>, type_id: <Integer>, primary_type: <String> }. You're missing a ${pokeParameters}.` })
+    }
   }
 
   database('pokemon').insert(newPokemon, '*')
 
-  .then(pokemon => response.status(201).json(pokemon))
+    .then(pokemon => response.status(201).json(pokemon))
 
-  .catch(error => response.status(500).json({ error }))
+    .catch(error => response.status(500).json({ error }))
 });
 
 
@@ -214,16 +214,16 @@ app.patch('/api/v1/pokemon/:region_id', checkAuth, (request, response) => {
   const { name, attack_power, defense_power, hp, power_total, type_id, primary_type } = request.body;
 
   if (!name && !attack_power && !defense_power && !hp && !power_total && !type_id && !primary_type) {
-    return response.status(422).send({ error: "Partial update failed, missing a parameter" })
+    return response.status(422).send({ error: 'Partial update failed, missing a parameter' })
   }
 
   database('pokemon').where({ region_id })
 
-  .update({ name, attack_power, defense_power, hp, power_total, type_id, primary_type }, '*')
+    .update({ name, attack_power, defense_power, hp, power_total, type_id, primary_type }, '*')
 
-  .then(patchedPokemon => response.status(200).json(patchedPokemon))
+    .then(patchedPokemon => response.status(200).json(patchedPokemon))
 
-  .catch(error => response.status(500).json({ error }));
+    .catch(error => response.status(500).json({ error }))
 });
 
 app.put('/api/v1/types/:id', checkAuth, (request, response) => {
@@ -231,49 +231,49 @@ app.put('/api/v1/types/:id', checkAuth, (request, response) => {
   const { type_label } = request.body;
 
   if (!type_label) {
-    return response.status(422).send({ error: "Update failed" })
+    return response.status(422).send({ error: 'Update failed' })
   }
 
   database('types').where({ id })
 
-  .update({ type_label }, '*')
+    .update({ type_label }, '*')
 
-  .then(putType => response.status(200).json(putType))
+    .then(putType => response.status(200).json(putType))
 
-  .catch(error => response.status(500).json({ error }));
+    .catch(error => response.status(500).json({ error }))
 });
 
 
 
 //DELETE Endpoints
-  //DELETE by pokemon region id
+//DELETE by pokemon region id
 app.delete('/api/v1/pokemon/:region_id', checkAuth, (request, response) => {
   const { region_id } = request.params;
 
   database('pokemon').where({ region_id }).del()
 
-  .then(response => {
-    if(!response) {
-      response.status(404).json({ error: 'Pokemon matching region id not found' })
-    }
-    response.sendStatus(204)
-  })
-  .catch( error => response.status(500).json({ error }) );
+    .then(response => {
+      if(!response) {
+        response.status(404).json({ error: 'Pokemon matching region id not found' })
+      }
+      response.sendStatus(204)
+    })
+    .catch(error => response.status(500).json({ error }) )
 });
 
-  //DELETE by pokemon id
+//DELETE by pokemon id
 app.delete('/api/v1/pokemon/id/:id', checkAuth, (request, response) => {
   const { id } = request.params;
 
   database('pokemon').where({ id }).del()
 
-  .then(response => {
-    if(!response) {
-      response.status(404).json({ error: 'Pokemon matching id not found' })
-    }
-    response.sendStatus(204)
+    .then(response => {
+      if(!response) {
+        response.status(404).json({ error: 'Pokemon matching id not found' })
+      }
+      response.sendStatus(204)
   })
-  .catch( error => response.status(500).json({ error }) );
+  .catch(error => response.status(500).json({ error }) )
 });
 
 module.exports = app;
